b83c5c0567f065781e84c4571ce39e4179973fee
human-321 <donahue2a@students.manateeschools.net>
2025-03-26 13:27:42 -0400

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -2,7 +2,7 @@
 #include <cstdint>
 #include <iostream>
 
-matrix::matrix(int width,int height)
+matrix::matrix(std::uint8_t width,std::uint8_t height)
 {
     m_components = std::vector<std::vector<double>>(height, std::vector<double>(width,0) );
 }
@@ -11,11 +11,17 @@ matrix::matrix(std::vector<std::vector<double>> vec) { m_components = vec; }
 std::uint8_t matrix::getWidth()  const { return (m_components[0]).size(); }
 std::uint8_t matrix::getHeight() const { return (m_components   ).size(); }
 
+matrix& matrix::setComponent(std::uint8_t row,std::uint8_t column, double value) 
+{
+    m_components[row][column] = value;
+    return *this;
+}
+
 void matrix::print() const
 {
     for(std::uint8_t row {0}; row < getHeight(); ++row) {
         for(std::uint8_t column {0}; column < getWidth(); ++column) {
-            std::cout << m_components[row][column] << ' ';
+            std::cout << m_components[row][column] << ", ";
         }
         if(row < getHeight() - 1) {std::cout << '\n';}
     }
