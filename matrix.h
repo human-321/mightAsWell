90c825f6750925d1b15a12e130fc777815c12a81
human-321 <donahue2a@students.manateeschools.net>
2025-03-26 13:27:55 -0400

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -11,13 +11,17 @@ class matrix
         std::vector<std::vector<double>> m_components;
     
     public:
-        matrix(int width,int height);
+        matrix(std::uint8_t width,std::uint8_t height); // define a matrix of width columns and height rows full of zeros
         matrix(std::vector<std::vector<double>> vec);
 
         std::uint8_t getWidth()  const;
         std::uint8_t getHeight() const;
 
-        void print() const;
+        // sets the component value at spot also returns reference to the object so you can chain
+        // for example mat.setPart(sdklf).setPart(different sdklf) or mat.setComponent(1,1,.5).setComponent(2,2,3);
+        matrix& setComponent(std::uint8_t row,std::uint8_t column, double value);
+
+        void print() const; // simple bare bones display for testing
 };
 
 
