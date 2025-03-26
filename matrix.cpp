53cb9b977d0a572fcca86c656a9b781feba0261c
human-321 <donahue2a@students.manateeschools.net>
2025-03-26 13:17:33 -0400

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -2,25 +2,24 @@
 #include <cstdint>
 #include <iostream>
 
-matrix::matrix(std::uint8_t width,std::uint8_t height)
+matrix::matrix(int width,int height)
 {
     m_components = std::vector<std::vector<double>>(height, std::vector<double>(width,0) );
 }
 matrix::matrix(std::vector<std::vector<double>> vec) { m_components = vec; }
 
-std::uint8_t matrix::getWidth()  { std::size(m_components[0]); }
-std::uint8_t matrix::getHeight() { std::size(m_components);    }
+std::uint8_t matrix::getWidth()  const { return (m_components[0]).size(); }
+std::uint8_t matrix::getHeight() const { return (m_components   ).size(); }
 
-void matrix::print()
+void matrix::print() const
 {
-    for(std::uint8_t row {0}; row <= getHeight(); ++row) {
-        for(std::uint8_t column {0}; column <= getWidth(); ++column) {
+    for(std::uint8_t row {0}; row < getHeight(); ++row) {
+        for(std::uint8_t column {0}; column < getWidth(); ++column) {
             std::cout << m_components[row][column] << ' ';
         }
-        if(row < getHeight() - 1) {std::cout << '\n'}
+        if(row < getHeight() - 1) {std::cout << '\n';}
     }
 }
-
 
 
 
